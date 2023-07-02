c9cfba9ecdb06e9536a4a161bb71627cc943b57e
Joe Cooper <swelljoe@gmail.com>
2023-07-02 18:34:31 -0500

    First poke at RPM build

---
+++ build-devel-rpm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Build and publish a package, called by github actions
+
+# echo, exit on error/undefined vars
+set -xeu
+
+MOD='jailkit'
+NAME="wbm-$MOD"
+
+# Always increasing. Also a human-readable datetime.
+BUILD=$(date +'%Y%m%d%H%M')
+
+# Load module.info to get version
+version=$(grep version $MOD/module.info | cut -d'=' -f2)
+VERSION="${version}.devel.${BUILD}"
+
+if [ -f epoch ]; then
+  epoch="--epoch $(cat epoch)"
+else
+  epoch=""
+fi
+
+mkdir tmp
+perl makemodulerpm.pl --rpm-depends --licence 'GPLv3' --email 'joe@virtualmin.com' --allow-overwrite --target-dir tmp "$epoch" "$MOD" "$VERSION"
+mv "tmp/${NAME}_${VERSION}_all.deb" .
+
+# Publish to pulp, I guess?
