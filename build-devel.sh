914f1c66fd389995821cce74992c8c27d386d918
Joe Cooper <swelljoe@gmail.com>
2023-07-02 00:18:13 -0500

    Let's build and publish with a shell script

---
+++ build-devel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Build and publish a package, called by github actions
+
+MOD='jailkit'
+NAME="webmin-$MOD"
+
+# Always increasing. Also a human-readable datetime.
+BUILD=$(date +'%Y%m%d%H%M')
+
+# Load module.info to get version
+. jailkit/module.info
+VERSION="${version}_devel_${BUILD}"
+
+mkdir tmp
+perl makemoduledeb.pl --deb-depends --licence 'GPLv3' --email 'joe@virtualmin.com' --allow-overwrite --target-dir tmp "$MOD" "$VERSION"
+
+# Publish to aptly
+curl --user $APTLY_USER:$APTLY_PASSWD -X POST -F file=@${NAME}_${VERSION}_all.deb https://aptly.virtualmin.com/api/files/${NAME}_${VERSION}
+#curl --user $APTLY_USER:$APTLY_PASSWD -X POST https://aptly.virtualmin.com/api/repos/virtualmin-7-gpl-devel/file/${NAME}_${VERSION}
+#curl --user $APTLY_USER:$APTLY_PASSWD https://aptly.virtualmin.com/api/publish/filesystem:7-gpl/virtualmin-devel
