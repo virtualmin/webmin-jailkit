12b46e98b4ad97230889c6611c31126c87a2a894
Joe Cooper <swelljoe@gmail.com>
2023-07-02 20:53:59 -0500

    Running as 'runner', but need noarch dir, too

--- build-devel-rpm.sh
+++ build-devel-rpm.sh
@@ -23,6 +23,7 @@ fi
 mkdir tmp
 # FIXME after PR is merged to Webmin
 mkdir -p "${HOME}/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}"
+mkdir -p "${HOME}/rpmbuild/RPMS/noarch"
 perl makemodulerpm.pl --rpm-depends --licence 'GPLv3' --allow-overwrite --target-dir tmp $epoch "$MOD" "$VERSION"
 mv "tmp/${NAME}_${VERSION}_all.deb" .
 
