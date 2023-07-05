0c4f2cc4036e5ad13505da241b7769734a5c3d8c
Joe Cooper <swelljoe@gmail.com>
2023-07-05 15:36:37 -0500

    Include full path, so we can sign packages, too.

--- build-stable-rpm.sh
+++ build-stable-rpm.sh
@@ -28,5 +28,5 @@ perl makemodulerpm.pl --rpm-depends --licence 'GPLv3' --allow-overwrite $epoch "
 # Copy to build/deploy server
 scp -i "${HOME}/.ssh/id_ed25519" "${HOME}/rpmbuild/RPMS/noarch/${NAME}-${VERSION}-1.noarch.rpm" "$BUILD_SSH_USER@build.virtualmin.com:/home/build/result/vm/7/gpl-devel/rpm/noarch"
 # Add it to the publish queue
-ssh -i "${HOME}/.ssh/id_ed25519" "$BUILD_SSH_USER@build.virtualmin.com" "flock /home/build/rpm-publish-queue -c 'echo vm/7/gpl-devel/rpm/noarch >> /home/build/rpm-publish-queue'"
+ssh -i "${HOME}/.ssh/id_ed25519" "$BUILD_SSH_USER@build.virtualmin.com" "flock /home/build/rpm-publish-queue -c 'echo vm/7/gpl-devel/rpm/noarch/${NAME}-${VERSION}-1.noarch.rpm >> /home/build/rpm-publish-queue'"
 
